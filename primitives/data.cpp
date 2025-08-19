b00fabfe0ec710322c196fdad3a326af407ffa24
Anukalp-dev <anukalp.tech@gmail.com>
2025-08-19 11:02:41 +0530

    Add initial implementation of data.cpp with variable declarations and output

---
+++ primitives/data.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int employees = 50;
+    int projects = 15;
+    float hoursToComplete = 40.5;
+    double priceOfProjects = 3999.99;
+    char projectsGrade = 'A';
+    bool isProjectsReady = true;
+
+    cout << isProjectsReady << endl;
+
+    return 5;
+}
