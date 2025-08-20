77c29d97bc5709129cfe39adf33bcf55755d681b
Anukalp-dev <anukalp.tech@gmail.com>
2025-08-20 07:32:01 +0530

    Add comment to clarify usage of escape character in strings.cpp

--- primitives/strings.cpp
+++ primitives/strings.cpp
@@ -7,6 +7,7 @@ int main() {
     string favFood = "Pizza\n";
     string favDrink = "Coke is my \"favorite\" drink."; 
     // i want to highlight favorite so here is a new character called ESCAPE CHARACTER '\'
+    // Go to microsoft docs for Cpp to learn more about escape charracter
 
     cout << favFood << favDrink << endl;
     // we will add '\n' for new line
